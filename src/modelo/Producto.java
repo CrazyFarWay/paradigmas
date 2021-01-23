812be6db6944f8420f48775f24e69a1543cc9933
ValeSaccone <valesaccone13@gmail.com>
2021-01-22 21:04:31 -0300

    Modificar base de datos y modelo

--- src/modelo/Producto.java
+++ src/modelo/Producto.java
@@ -1,6 +1,61 @@
 package modelo;
 
 public class Producto {
+
+    String nombre, marca;
+    double precio;
+    int id, cantidad;
+
+    public Producto(int id, String nombre, String marca, double precio, int cantidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.marca = marca;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+
+    public Producto() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
     
     
     
